3dfd2c197e3c065809db4aff12735b633bb74fed
antara-walid <Antara.walid.27@gmail.com>
2022-10-23 13:59:44 +0100

    finish Node class first version

--- src/main/java/org/example/Node.java
+++ src/main/java/org/example/Node.java
@@ -2,24 +2,58 @@ package org.example;
 
 public class Node {
     private int data;
-    private Node left ;
-    private Node Right;
+    private Node left;
+    private Node right;
+
     // constructor
-    private Node(int data)
-    {
+    private Node(int data) {
         this.data = data;
     }
 
     //methods
     //1.insert
-    public void insert(int data)
-    {
-        // create the node
-        Node node = new Node(data);
+    public void insert(int data) {
+        if (data < this.data) {
+            //inser in left
+            if (left == null)
+                left = new Node(data);
+            else
+                left.insert(data);
+        } else {
+            //insert in right
+            if (right == null)
+                right = new Node(data);
+            else
+                right.insert(data);
+        }
 
     }
 
-
     //2.contains
+    public boolean contains(int data) {
+        if (this.data == data)
+            return true;
+        else {
+            if (data < this.data) {
+                if (left == null)
+                    return false;
+                return left.contains(data);
+            } else {
+                if (right == null)
+                    return true;
+                return right.contains(data);
+            }
+
+        }
+    }
     //3.in-orderPrint
+    public void inOrderPrint()
+    {
+        if(left != null)
+            left.inOrderPrint();
+        System.out.println(this.data);
+        if(right != null)
+            right.inOrderPrint();
+
+    }
 }
