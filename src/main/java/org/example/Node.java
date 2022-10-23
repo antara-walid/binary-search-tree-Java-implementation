e1614ca56525f6a577e06f4aa28d67391e7433fc
antara-walid <Antara.walid.27@gmail.com>
2022-10-23 13:43:06 +0100

    change BinaryTree Class to Node

---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Node {
+    private int data;
+    private Node left ;
+    private Node Right;
+    // constructor
+    private Node(int data)
+    {
+        this.data = data;
+    }
+
+    //methods
+    //1.insert
+    public void insert(int data)
+    {
+        // create the node
+        Node node = new Node(data);
+
+    }
+
+
+    //2.contains
+    //3.in-orderPrint
+}
